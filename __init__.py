063fdb15c59dc00d94d01d0e72b574720621d7e1
Tom Louden <t.m.louden@warwick.ac.uk>
2015-05-14 12:17:31 +0100

    first commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+ This is a module with some routines designed to help the analysis of spotty transits by Tom Louden
+
+ tml.spot_model    -- calls on ATLAS to simulate the effect of adding spots to a transit.
+
+"""
+
+from spot_model import *
+
+#
